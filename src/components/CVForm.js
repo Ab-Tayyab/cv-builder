7c256d53ac3d1e72dddc6b480af62bbdc8b890e5
abdullahtayyab894 <abdullahtayyab414@gmail.com>
2024-09-14 01:43:12 +0500

    layout changing

--- src/components/CVForm.js
+++ src/components/CVForm.js
@@ -3,7 +3,7 @@ import { useForm, useFieldArray } from "react-hook-form";
 import "./CVForm.css";
 
 const CVForm = ({ onSubmit }) => {
-  const { register, control, handleSubmit } = useForm({
+  const { register, control, handleSubmit, watch } = useForm({
     defaultValues: {
       experience: [{ jobTitle: "", company: "", duration: "" }],
       skills: [{ name: "" }],
@@ -28,8 +28,12 @@ const CVForm = ({ onSubmit }) => {
     name: "skills",
   });
 
-  // Local state to store the uploaded image
+  // Local state for multi-page functionality
+  const [step, setStep] = useState(1);
   const [image, setImage] = useState(null);
+
+  // Watch all form fields
+  const formValues = watch();
 
   // Handle image upload
   const handleImageUpload = (e) => {
@@ -37,94 +41,193 @@ const CVForm = ({ onSubmit }) => {
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        setImage(reader.result); // Convert image to base64
+        setImage(reader.result);
       };
       reader.readAsDataURL(file);
     }
   };
 
+  const nextPage = () => setStep((prev) => prev + 1);
+  const prevPage = () => setStep((prev) => prev - 1);
+
+  const isStepValid = () => {
+    // Validate fields for each step
+    if (step === 1) {
+      return formValues.name && formValues.email && formValues.phone;
+    } else if (step === 2) {
+      return formValues.degree && formValues.institution && formValues.year;
+    } else if (step === 3) {
+      return formValues.experience.some(
+        (exp) => exp.jobTitle && exp.company && exp.duration
+      );
+    } else if (step === 4) {
+      return formValues.skills.some((skill) => skill.name);
+    }
+    return true;
+  };
+
   const onFormSubmit = (data) => {
-    const formData = { ...data, profileImage: image }; // Add the image to the form data
-    onSubmit(formData); // Pass the form data, including the image, to the parent component
+    const formData = { ...data, profileImage: image };
+    onSubmit(formData);
   };
 
   return (
-    <form onSubmit={handleSubmit(onFormSubmit)} className="cv-form">
-      <label>Profile Picture:</label>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
+    <div>
+      <form onSubmit={handleSubmit(onFormSubmit)} className="cv-form">
+        {step === 1 && (
+          <div className="cv-container">
+            <h2>Personal Information</h2>
+            <label>Profile Picture:</label>
+            <br />
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              className="cv-image"
+            />
+            {image && (
+              <div className="image-preview">
+                <img
+                  src={image}
+                  alt="Profile Preview"
+                  style={{
+                    width: "150px",
+                    height: "150px",
+                    borderRadius: "50%",
+                  }}
+                />
+              </div>
+            )}
+            <br />
+            <label>Name:</label>
+            <br />
+            <input type="text" {...register("name")} />
+            <br />
+            <label>Email:</label>
+            <br />
+            <input type="email" {...register("email")} />
+            <br />
+            <label>Phone:</label>
+            <br />
+            <input type="tel" {...register("phone")} />
+          </div>
+        )}
 
-      {image && (
-        <div className="image-preview">
-          <img
-            src={image}
-            alt="Profile Preview"
-            style={{ width: "150px", height: "150px", borderRadius: "50%" }}
-          />
+        {step === 2 && (
+          <div className="cv-container">
+            <h2>Education</h2>
+            <label>Degree:</label>
+            <br />
+            <input type="text" {...register("degree")} />
+            <br />
+            <label>Institution:</label>
+            <br />
+            <input type="text" {...register("institution")} />
+            <br />
+            <label>Year:</label>
+            <br />
+            <input type="text" {...register("year")} />
+          </div>
+        )}
+
+        {step === 3 && (
+          <div className="cv-container">
+            <h2>Experience</h2>
+            {experienceFields.map((item, index) => (
+              <div key={item.id}>
+                <label>Job Title:</label>
+                <br />
+                <input
+                  type="text"
+                  {...register(`experience.${index}.jobTitle`)}
+                />
+                <br />
+                <label>Company:</label>
+                <br />
+                <input
+                  type="text"
+                  {...register(`experience.${index}.company`)}
+                />
+                <br />
+                <label>Duration:</label>
+                <br />
+                <input
+                  type="text"
+                  {...register(`experience.${index}.duration`)}
+                />
+                <button
+                  type="button"
+                  onClick={() => removeExperience(index)}
+                  disabled={experienceFields.length === 1}
+                  className="remove-btn"
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
+            <button
+              type="button"
+              onClick={() =>
+                addExperience({ jobTitle: "", company: "", duration: "" })
+              }
+              className="add-btn"
+            >
+              Add Experience
+            </button>
+          </div>
+        )}
+
+        {step === 4 && (
+          <div className="cv-container">
+            <h2>Skills</h2>
+            {skillFields.map((item, index) => (
+              <div key={item.id}>
+                <input type="text" {...register(`skills.${index}.name`)} />
+                <button
+                  type="button"
+                  onClick={() => removeSkill(index)}
+                  disabled={skillFields.length === 1}
+                  className="remove-btn"
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
+            <button
+              type="button"
+              onClick={() => addSkill({ name: "" })}
+              className="add-btn"
+            >
+              Add Skill
+            </button>
+          </div>
+        )}
+
+        {/* Button Container for Navigation */}
+        <div className="button-container">
+          {step > 1 && (
+            <button type="button" onClick={prevPage} className="prev-btn">
+              Previous
+            </button>
+          )}
+          {step < 4 && (
+            <button
+              type="button"
+              onClick={nextPage}
+              disabled={!isStepValid()}
+              className="next-btn"
+            >
+              Next
+            </button>
+          )}
+          {step === 4 && (
+            <button type="submit" disabled={!isStepValid()} className="gen-btn">
+              Generate CV
+            </button>
+          )}
         </div>
-      )}
-      <h2>Personal Information</h2>
-      <label>Name:</label>
-      <input type="text" {...register("name")} />
-      <label>Email:</label>
-      <input type="email" {...register("email")} />
-      <label>Phone:</label>
-      <input type="tel" {...register("phone")} />
-
-      <h2>Education</h2>
-      <label>Degree:</label>
-      <input type="text" {...register("degree")} />
-      <label>Institution:</label>
-      <input type="text" {...register("institution")} />
-      <label>Year:</label>
-      <input type="text" {...register("year")} />
-
-      <h2>Experience</h2>
-      {experienceFields.map((item, index) => (
-        <div key={item.id}>
-          <label>Job Title:</label>
-          <input type="text" {...register(`experience.${index}.jobTitle`)} />
-          <label>Company:</label>
-          <input type="text" {...register(`experience.${index}.company`)} />
-          <label>Duration:</label>
-          <input type="text" {...register(`experience.${index}.duration`)} />
-          <button
-            type="button"
-            onClick={() => removeExperience(index)}
-            disabled={experienceFields.length === 1}
-          >
-            Remove Experience
-          </button>
-        </div>
-      ))}
-      <button
-        type="button"
-        onClick={() =>
-          addExperience({ jobTitle: "", company: "", duration: "" })
-        }
-      >
-        Add Experience
-      </button>
-
-      <h2>Skills</h2>
-      {skillFields.map((item, index) => (
-        <div key={item.id}>
-          <input type="text" {...register(`skills.${index}.name`)} />
-          <button
-            type="button"
-            onClick={() => removeSkill(index)}
-            disabled={skillFields.length === 1}
-          >
-            Remove Skill
-          </button>
-        </div>
-      ))}
-      <button type="button" onClick={() => addSkill({ name: "" })}>
-        Add Skill
-      </button>
-      <br />
-
-      <button type="submit">Generate CV</button>
-    </form>
+      </form>
+    </div>
   );
 };
 
--- src/components/CVPreview.js
+++ src/components/CVPreview.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './CVPreview.css';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
+import React from "react";
+import "./CVPreview.css";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 const CVPreview = ({ data }) => {
   const downloadPDF = () => {
     // Select the CV container
-    const cvElement = document.querySelector('.cv-preview');
-    
+    const cvElement = document.querySelector(".cv-preview");
+
     html2canvas(cvElement).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
+      const imgData = canvas.toDataURL("image/png");
       const pdf = new jsPDF();
 
       // Calculate the dimensions of the PDF based on the image dimensions
@@ -25,35 +25,76 @@ const CVPreview = ({ data }) => {
       const imgScaledWidth = imgWidth * ratio;
       const imgScaledHeight = imgHeight * ratio;
 
-      pdf.addImage(imgData, 'PNG', 0, 0, imgScaledWidth, imgScaledHeight);
-      pdf.save('CV.pdf');
+      pdf.addImage(imgData, "PNG", 0, 0, imgScaledWidth, imgScaledHeight);
+      pdf.save("CV.pdf");
     });
   };
 
   return (
     <div className="cv-preview">
-      {/* Profile Image */}
-      {data.profileImage && (
-        <img src={data.profileImage} alt="Profile" className="profile-image" />
-      )}
-      <h1>{data.name}</h1>
-      <p>Email: {data.email}</p>
-      <p>Phone: {data.phone}</p>
+      <div className="cv-about">
+        <div>
+          <h1>{data.name}</h1>
+          <p>Email: {data.email}</p>
+          <p>Phone: {data.phone}</p>
+        </div>
+        <div>
+          {data.profileImage && (
+            <img
+              src={data.profileImage}
+              alt="Profile"
+              className="profile-image"
+            />
+          )}
+        </div>
+      </div>
+      <div className="cv-summary">
+        <p>Summary</p>
+      </div>
+      <div className="cv-experience">
+        <h2>Experience</h2>
+        {data.experience &&
+          data.experience.map((exp, index) => (
+            <div className="exp-container">
+              <div className="exp-child1">
+                <h1>{exp.duration}</h1>
+              </div>
+              <div className="exp-child2">
+                <h2>{exp.jobTitle}</h2>
+                <h4>{exp.company}</h4>
+                <p>Summary</p>
+              </div>
+              {/* <p key={index}>
+                {exp.jobTitle} at {exp.company} ({exp.duration})
+              </p> */}
+            </div>
+          ))}
+      </div>
+      <div className="cv-skills">
+        <h2>Skills</h2>
+        {data.skills &&
+          data.skills.map((skill, index) => (
+            <li key={index} className="skill-tag">
+              {skill.name}
+            </li>
+          ))}
+      </div>
+      <div className="cv-education">
+        <h2>Education</h2>
+        <div className="edu-container">
+          <div className="edu-child1">
+            <h1>{data.year}</h1>
+          </div>
+          <div className="edu-child2">
+            <h2>{data.degree}</h2>
+            <h3>{data.institution}</h3>
+          </div>
+        </div>
+        {/* <p>
+          {data.degree} from {data.institution} ({data.year})
+        </p> */}
+      </div>
 
-      <h2>Education</h2>
-      <p>{data.degree} from {data.institution} ({data.year})</p>
-
-      <h2>Experience</h2>
-      {data.experience && data.experience.map((exp, index) => (
-        <p key={index}>{exp.jobTitle} at {exp.company} ({exp.duration})</p>
-      ))}
-
-      <h2>Skills</h2>
-      {data.skills && data.skills.map((skill, index) => (
-        <span key={index} className="skill-tag">{skill.name}</span>
-      ))}
-
-      {/* Download Button */}
       <button onClick={downloadPDF} className="download-btn">
         Download PDF
       </button>
